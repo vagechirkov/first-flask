c24bc9a3a4c431cd9f2cc0a34c4c23108e863183
vagechirkov <vagechirkov@gmail.com>
2021-12-04 11:01:37 +0100

    add step 5

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,38 @@
-from flask import Flask
+import os
+import json
+from flask import Flask, jsonify, request, Response
+import pandas as pd
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route("/hello/")
+wines = [
+    {"id": 0, "alcohol": 8, "quality": 10},
+    {"id": 1, "alcohol": 12, "quality": 8},
+    {"id": 2, "alcohol": 10.5, "quality": 9},
+]
+
+module_dir = os.path.abspath(os.path.dirname(__file__))
+file_path = os.path.join(module_dir, "winequality-white.csv")
+df = pd.read_csv(file_path, sep=";")
+
+
+@app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, from TechLabs!</p>"
+
+
+@app.route("/api/wines/all", methods=["GET"])
+def return_all():
+    return Response(df.to_json(orient="index"), mimetype="application/json")
+
+
+@app.route("/api/wines", methods=["GET"])
+def get_wine_by_id():
+    if "id" in request.args:
+        id = int(request.args["id"])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    row = df.iloc[[id]]
+    return Response(row.to_json(orient="index"), mimetype="application/json")
